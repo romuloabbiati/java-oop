d30def5aa94a34d73d1b4a641d5ec8cf5fe43802
romuloabbiati <rhabbiati@hotmail.com>
2025-01-31 06:04:57 +0000

    how to print double quotes or other characters

--- Module2/OiMundo.java
+++ Module2/OiMundo.java
@@ -1,15 +1,7 @@
 public class OiMundo {
 
   public static void main(String[] args) {
-    // isto eh um comentario de uma linha
-    /*
-      isto eh um comentario de multiplas linhas
-      estah linha serah ignorada pelo compilador
-      System.out.println("essa instrucao serah ignorada pelo compilador");
-      e essa tambem
-    */
-
-     System.out.println("Oi mundo");
+     System.out.println("Oi \nmundo");
 
   }
 
