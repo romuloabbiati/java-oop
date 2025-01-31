2845d59add5e4399dbeb48f56481e7a78ef5d7ff
romuloabbiati <rhabbiati@hotmail.com>
2025-01-31 05:58:26 +0000

    about how to use comments in java

--- Module2/OiMundo.java
+++ Module2/OiMundo.java
@@ -1,7 +1,15 @@
 public class OiMundo {
 
   public static void main(String[] args) {
-    System.out.println("Oi mundo");
+    // isto eh um comentario de uma linha
+    /*
+      isto eh um comentario de multiplas linhas
+      estah linha serah ignorada pelo compilador
+      System.out.println("essa instrucao serah ignorada pelo compilador");
+      e essa tambem
+    */
+
+     System.out.println("Oi mundo");
 
   }
 
