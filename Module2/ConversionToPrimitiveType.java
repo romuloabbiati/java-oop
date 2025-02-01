57d5d97e90803505a8879d92bf7e0463cd10ff1f
romuloabbiati <rhabbiati@hotmail.com>
2025-02-01 10:43:07 +0000

    conversion between primitive types or also called casting

---
+++ Module2/ConversionToPrimitiveType.java
@@ -0,0 +1,11 @@
+public class ConversionToPrimitiveType {
+
+  public static void main(String[] args) {
+    double largura = 100.0;
+
+    int tamanho = (int) largura;
+    System.out.println(tamanho);
+
+  }
+
+}
