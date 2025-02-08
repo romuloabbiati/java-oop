56ab45620fab4830db23185ad84c9691924dd143
romuloabbiati <rhabbiati@hotmail.com>
2025-02-08 03:27:24 +0000

    how to use methods

--- MeuPrimeiroProjeto/src/dft/Carro.java
+++ MeuPrimeiroProjeto/src/dft/Carro.java
@@ -6,7 +6,12 @@ public class Carro {
 	String modelo;
 	String cor;
 	int anoDeFabricacao;
+	boolean biCombustivel;
 	
 	Proprietario dono;
+	
+	void ligar() {
+		System.out.println("Ligando o carro: " + modelo);
+	}
 
 }
